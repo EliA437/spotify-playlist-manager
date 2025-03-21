70abd635bfd71aa03fd2788912b116a694f51045
EliA437 <allenmwnc.eli@gmail.com>
2025-03-21 15:44:17 -0500

    added more fucntions for analyze file

--- analyze_data.py
+++ analyze_data.py
@@ -11,14 +11,31 @@ with open('User Data/TopArtists.txt') as file:
     top_artists_contents = file.read()
 #print(top_artists_contents)
 
-prompt = 'What does this say about me if these are my top artists'
+prompt1 = f'What does this say about me if these are my top artists: {top_artists_contents}'                     
 
-completion = openai.ChatCompletion.create(
+num_artists_recomended = 100
+prompt2 = f'Can you recomend me {num_artists_recomended} more artists that I might like based on: {top_artists_contents}'
+
+prompt3 = f'Can you guess my gender, age, and what region im from based on my top artist{top_artists_contents}'
+
+def open_ai_api_req(promp):
+    response = openai.ChatCompletion.create(
     model="gpt-4o",
     messages=[{
         "role": "user",
-        "content": f"{prompt}: {top_artists_contents}"        # Input to ChatGpt
+        "content": f"{promp}"        # Input to ChatGpt
     }]
-)
+    )
+    return response
 
-print(completion.choices[0].message.content + '\n')
+print('What your top artists say about you: \n\n')
+completion_0 = open_ai_api_req(prompt1)
+print(completion_0.choices[0].message.content + '\n')
+
+print('Giving recomendations... \n\n')
+completion_1 = open_ai_api_req(prompt2)
+print(completion_1.choices[0].message.content + '\n')
+
+print('Guessing age, gender, and region... \n\n')
+completion_2 = open_ai_api_req(prompt3)
+print(completion_2.choices[0].message.content + '\n')
